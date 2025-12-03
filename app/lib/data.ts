91e60823f798382df7e0f763c7bba19316690864
zehuangjack <suqiuliang727@gmail.com>
2025-12-04 07:19:32 +0800

    feat(exception): handle exceptions for invoices

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -159,7 +159,8 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-
+    
+    console.log(invoice);
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
