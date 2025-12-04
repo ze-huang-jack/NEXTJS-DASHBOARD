abf416f275cb0400b94472c5d25d1773a84ad70a
zehuangjack <suqiuliang727@gmail.com>
2025-12-04 12:04:18 +0800

    fix(invoice): correct type error in `createInvoice`

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -34,8 +34,7 @@ export async function createInvoice(formData: FormData) {
       `;
     } catch (error) {
         console.error(error);
-        return {
-            message: 'Database Error: Failed to Create Invoice.'
+        throw new Error('Database Error: Failed to Create Invoice.')
         }
     }
 
